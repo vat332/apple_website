34f3bb6bc74282ae4a724a9f6351c799c0a9ab9f
vat332 <oporowymakus21@gmail.com>
2025-05-09 12:50:43 +0200

    Add bag and search SVG icons, update Navbar to use new images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { appleImg } from "../utils";
+import { appleImg, bagImg, searchImg } from "../utils";
 const Navbar = () => {
   return (
     <header>
       <nav>
-        <img src={appleImg} alt="apple_logo" />
+        <img src={appleImg} alt="apple_logo" width={14} height={18} />
+        <div>
+          {["Smartfony", "MacBooki", "Tablety"].map((nav) => (
+            <div key={nav}>{nav}</div>
+          ))}
+        </div>
+        <div>
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
       </nav>
     </header>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
 import apple from "/assets/images/apple.svg";
+import bag from "/assets/images/bag.svg";
+import search from "/assets/images/search.svg";
 
 export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
