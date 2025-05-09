2e99b328f95a863b6dabb239bc71224c47a714e2
vat332 <oporowymakus21@gmail.com>
2025-05-09 12:13:52 +0200

    Initialize React project with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque maxime
+        adipisci voluptate iure quae veniam odio facere perspiciatis id?
+        Voluptatem vel optio, reiciendis odit dolorum nihil tempora suscipit
+        natus a.
+      </p>
+    </div>
+  );
+};
+
+export default App;
