49362efa84cd6049195e883042fe760da4eaa9c9
vat332 <oporowymakus21@gmail.com>
2025-05-10 13:34:07 +0200

    Refactor Hero component to handle video source changes on window resize

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,25 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { heroVideo, smallHeroVideo } from "../utils";
 const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(
     window.innerWidth < 760 ? smallHeroVideo : heroVideo
   );
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
