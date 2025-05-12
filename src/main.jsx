3dc436f77d1fb93579b96456a671d49880c6dbfe
vat332 <oporowymakus21@gmail.com>
2025-05-12 13:57:09 +0200

    enhance Sentry integration with performance monitoring settings

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,14 @@ import "./index.css";
 
 Sentry.init({
   dsn: "https://78dcd5f8c003e7dfe6aed8f49c147015@o4508967582367744.ingest.de.sentry.io/4509309897932880",
+  integrations: [Sentry.browserTracingIntegration()],
 
-  sendDefaultPii: true,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost"],
 });
 
 createRoot(document.getElementById("root")).render(
